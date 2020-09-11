6eae243665022a4787dc54128e249b4c5772bab3
李铭昕 <715557344@qq.com>
2020-09-11 14:43:07 +0800

    Fixed bug

--- config/routes.php
+++ config/routes.php
@@ -17,4 +17,4 @@ Router::post('/user/login', App\Controller\UserController::class . '::login');
 Router::post('/user/wxlogin', App\Controller\UserController::class . '::wxlogin');
 Router::get('/user/info', App\Controller\UserController::class . '::info');
 Router::post('/user/finish-register', App\Controller\UserController::class . '::finishRegister');
-Router::post('/user/save', App\Controller\UserController::class . '::userSave');
+Router::post('/user/save', App\Controller\UserController::class . '::save');
