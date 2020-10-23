9857fcf952b46e7907bbe081463bb4e83ede349f
李铭昕 <715557344@qq.com>
2020-10-23 11:13:10 +0800

    更新欢迎语

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -15,12 +15,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $user = $this->request->input('user', 'Hyperf');
-        $method = $this->request->getMethod();
-        return $this->response->success([
-            'user' => $user,
-            'method' => $method,
-            'message' => 'Hello Hyperf.',
-        ]);
+        return $this->response->success('Hello SignAPI.');
     }
 }
